f08d925029d322e71d4ed21d5c108770f89d55b4
root <6772@holbertonstudents.com>
2023-06-15 10:25:17 -0700

    task 2

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,1 +1,21 @@
+#include<stdio.h>
 
+/**
+ * main - Prints Write a program that prints the alphabet in 
+ * lowercase, followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+	char alphabet = 'a';
+	do
+	{
+	putchar(alphabet);
+	putchar;
+	alphabet++;
+	} while (alphabet <= 'z');
+	return (0);
+}
