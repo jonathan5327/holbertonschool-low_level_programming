2c73ad3181fdf4f1cc6ca919589387040a270bba
root <6772@holbertonstudents.com>
2023-06-15 11:04:15 -0700

    task 3

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+/**
+ *
+ */
+
+int main(void)
+{
+	
+do {
+	char alphamay = 'A';
+	putchar(alphamay);
+	alphamay++;
+	(alphamay <= 'Z');
+	putchar ('\n');
+	return (0);
+
+	char alphamin = 'a';
+	putchar(alphamin);
+        alphamin++;
+        (alphamin <= 'z');
+        putchar ('\n');
+} while
+
+	return (0);
+}
