9a231c77aef0e19a2498950b5ca161a6bee024b7
root <6772@holbertonstudents.com>
2023-06-16 15:09:10 -0700

    task 2

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,14 +1,18 @@
 #include "main.h"
 /**
- *
- *
+ *print_alphabet_x10 - prints Write a function that prints 10 times the alphabe
+ *t, in lowercase, followed by a new line.
+ *Return: Always 0 (Success)
  */
 void print_alphabet_x10(void);
 {
-	char letter;
+	int num = 0;
+	int letter;
+	while (num++ <= 9)
+{
 	for (letter = 'a'; letter <= 'z'; letter++)
-	{
 		_putchar(letter);
-	}	
 
+	_putchar(10);
+}
 }
