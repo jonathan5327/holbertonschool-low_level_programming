0807ab4d20e2b703fe15a488e3b424f6fb1e7b7c
root <6772@holbertonstudents.com>
2023-06-16 15:22:02 -0700

    task 2

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -2,17 +2,17 @@
 /**
  *print_alphabet_x10 - prints Write a function that prints 10 times the alphabe
  *t, in lowercase, followed by a new line.
- *Return: Always 0 (Success)
+ *Return: no return
  */
 void print_alphabet_x10(void)
 {
-	int num = 0;
-	int letter;
-	while (num++ <= 9)
+int num = 0;
+int letter;
+while (num++ <= 9)
 {
-	for (letter = 'a'; letter <= 'z'; letter++)
-		_putchar(letter);
+for (letter = 'a'; letter <= 'z'; letter++)
+_putchar(letter);
 
-	_putchar(10);
+_putchar(10);
 }
 }
