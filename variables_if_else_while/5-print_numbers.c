2d0ff187dcfaf0618f35b6eae2c2e24e3cbcaef9
root <6772@holbertonstudents.com>
2023-06-15 18:01:14 -0700

    task 5

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -3,7 +3,7 @@
  * main - Prints Write a program that prints all single digit numbers of base 1
  * 0 starting from 0, followed by a new line.
  *
- * Return Always 0
+ * Return: Always 0
  */
 int main(void)
 {
