0e3b7600e5c48c11939cbd9c715588e0bd679040
root <6772@holbertonstudents.com>
2023-06-16 05:14:16 -0700

    Task 7

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *main - Prints Write a program that prints the lowercase alphabet in reverse, 
+ *followed by a new line.
+ *Return: Always 0
+ */
+
+int main(void)
+{
+	char alphabet = 'z';
+
+do {
+
+	putchar(alphabet);
+	alphabet--; } while (alphabet >='a');
+		putchar ('\n');
+	return 0;
+
+}
