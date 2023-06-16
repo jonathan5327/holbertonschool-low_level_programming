4add0cb7286ebbab4b7978ff52e6df4622716fab
root <6772@holbertonstudents.com>
2023-06-15 17:54:07 -0700

    task 5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - Prints Write a program that prints all single digit numbers of base 1
+ * 0 starting from 0, followed by a new line.
+ *
+ * Return Always 0
+ */
+int main(void)
+	{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		printf("%d", num)
+
+	printf("\n")
+
+	return (0);
+	}
