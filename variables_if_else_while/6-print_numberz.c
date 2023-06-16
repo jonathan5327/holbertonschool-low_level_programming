fb6281ffe0f4a307fde650519efb24cd583d5177
root <6772@holbertonstudents.com>
2023-06-16 04:39:44 -0700

    6-print_numberz.c

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /*
- * main - Prints Write a program that prints all single digit numbers of base 1
+ *main - Prints Write a program that prints all single digit numbers of base 1
  * 0 starting from 0, followed by a new line.
  *
- * Return: Always 0
+ *Return: Always 0
 */
 int main(void)
 {
