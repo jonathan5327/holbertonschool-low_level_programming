f52aa5c370b8321e9aa81f905ea1ac5463ef620a
root <6772@holbertonstudents.com>
2023-06-16 09:35:53 -0700

    task 1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+int _putchar(char c)
+void print_alphabet(void);
+int main(void)
+{
+	char alphabet= 'a';
+
+	while(alphabet <='z')
+	{
+		putchar(alphabet);
+	alphabet++;
+	}
+	
+	putcar('\n');
+	return (0);
+}
