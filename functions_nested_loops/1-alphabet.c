da334c87c4357b91190593b08eb17165c0144b52
root <6772@holbertonstudents.com>
2023-06-16 10:29:10 -0700

    task 1

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
- *main - Prints_alphabet Write a program that prints _putchar, followed by a new line.
+ *print_alphabet - Write a program that prints _putchar,
+ *followed by a new line.
  *
  *Return: Always 0 (Success)
 */
