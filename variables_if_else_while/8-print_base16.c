e4862278d36d3180b68c1d1d010a1e394c613bad
root <6772@holbertonstudents.com>
2023-06-16 06:24:35 -0700

    Task 8

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -9,10 +9,10 @@ int main(void)
 	int num;
 	char let;
 
-	for (num = 0; num < 10; num++)
+for (num = 0; num < 10; num++)
 		putchar((num % 10) + '0');
 
-	for (let = 'a'; let <= 'f'; let++);
+for (let = 'a'; let <= 'f'; let++);
 		putchar(let);
 
 	putchar ('\n');
