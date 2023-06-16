d4a9366a04ce1e42967e929a88300f049fe8b32b
root <6772@holbertonstudents.com>
2023-06-16 05:16:22 -0700

    Task 7

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -2,7 +2,7 @@
 /**
  *main - Prints Write a program that prints the lowercase alphabet in reverse,
  *followed by a new line.
- *Return: Always 0
+ *Return: Always 0 (Success)
  */
 
 int main(void)
