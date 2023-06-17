a38ed26e2c78edd465212b9033690a22d94401fc
root <6772@holbertonstudents.com>
2023-06-16 19:58:04 -0700

    task 5

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *_abs - prints Write a function that prints the sign of a number.
+ *@i: number targeted
+ *Return: returns 0
+ */
+int print_sign(int n)
+{
+	if ( n > 0)
+	return (n);
+	else if (i < 0)
+	return (-n);
+	else
+	return (0);
+
+
+}
