42f061c3b4e53c86faa54e37cd7037af51be741a
root <6772@holbertonstudents.com>
2023-06-18 19:37:31 -0700

    task 3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *print_numbers - Write a function that prints the numbers, fr
+ *om 0 to 9, followed by a new line.
+ *Return: returns nothing
+ */
+void print_numbers(void)
+{
+	int number = 48;;
+	for (number = 48; number < 58; number++)
+	{
+	_putchar(number);
+	}
+_putchar(10);
+}
