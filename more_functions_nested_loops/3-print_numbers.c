253e3df3579232f1d76f8bf472a64991a92fd1bd
root <6772@holbertonstudents.com>
2023-06-18 19:54:11 -0700

    task 3

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -4,7 +4,7 @@
  *om 0 to 9, followed by a new line.
  *Return: returns nothing
  */
-void print_numbers(void);
+void print_numbers(void)
 {
 int number = 48;
 for (number = 48; number < 58; number++)
