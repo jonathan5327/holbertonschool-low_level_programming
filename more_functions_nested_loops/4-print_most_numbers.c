c7516d660aa6065eb15ac66d3d79b5709005c175
root <6772@holbertonstudents.com>
2023-06-18 20:20:17 -0700

    task 4

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "main.h"
 /**
- *print_most_numbers - Write a function that prints the numbers, from 0 
+ *print_most_numbers - Write a function that prints the numbers, from 0
  *to 9, followed by a new line.
  *
  *Return: Always 0
