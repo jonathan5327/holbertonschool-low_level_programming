1da91dce36625b9fec0aea69ac0aef4b0a045f01
root <6772@holbertonstudents.com>
2023-06-22 07:01:43 -0700

    task 5

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ *
+ */
+void rev_string(char *s)
+{
+	int tam = strlen(s);
+	int i;
+	char aux;
+
+	for(i=0;i<tam/2;i++){
+	aux = s[i];
+	s[i] = s[tam-1-i];
+	s[tam-i-1] = aux;
+	}
+}
+
