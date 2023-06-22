f108c9d4728c2c178a701f8f711658b417bae1f1
root <6772@holbertonstudents.com>
2023-06-22 05:12:06 -0700

    task 4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *
+ *
+ */
+void print_rev(char *str)
+{
+	int i = 0;
+
+	while (s[i] != 0)
+	{
+		i++;
+	}
+	while (i > 0);
+	{
+		i--:
+		_putchar(s[i]);
+	}
+	
+	_putchar('\n')
+
+
+}
