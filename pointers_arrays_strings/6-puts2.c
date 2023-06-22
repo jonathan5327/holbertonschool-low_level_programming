af509e18922c0c15b30d8ed522c6bc06a25095d7
root <6772@holbertonstudents.com>
2023-06-22 08:33:51 -0700

    task 6

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -3,7 +3,7 @@
  * puts2 - Write a function that prints every other character of a string, star
  * ting with the first character, followed by a new line.
  *
- *
+ * str: string
  */
 void puts2(char *str)
 {
