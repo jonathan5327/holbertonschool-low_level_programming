83c33c9ca4680f1374cced634576193b329ad0ec
root <6772@holbertonstudents.com>
2023-06-22 07:08:11 -0700

    task 5

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -11,11 +11,11 @@ void rev_string(char *s)
 	int i;
 	char aux;
 
-	for(i = 0; i < tam / 2; i++)
+	for (i = 0; i < tam / 2; i++)
 	{
 	aux = s[i];
-	s[i] = s[tam -1 -i];
-	s[tam -i -1] = aux;
+	s[i] = s[tam - 1 - i];
+	s[tam - i - 1] = aux;
 	}
 }
 
