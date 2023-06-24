99e2115b3d063ea66b950bbce9c4cbc35f98656c
root <6772@holbertonstudents.com>
2023-06-24 14:25:26 -0700

    task 1

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,7 +1,11 @@
 #include "main.h"
 #include <string.h>
 /***
- *
+ *_strncat - Write a function that concatenates two strings
+ *@dest: string
+ *@src: string
+ *@n: character
+ *Return: Returns new concatenated string
  *
  */
 char *_strncat(char *dest, char *src, int n)
@@ -9,15 +13,13 @@ char *_strncat(char *dest, char *src, int n)
 	int i = 0;
 	int j = 0;
 
-	while (dest[i] != '\0')
+	while (dest[i++])
 	{
-		i++;
-	}
-	for (j = 0; src[j] '\0' && j < n; i++)
-	{
-		dest[j] = src[i];
 		j++;
 	}
-	dest[i] = '\0'
+	for (i = 0; src[i] && i < n; i++)
+	{
+		dest[j++] = src[i];
+	}
 		return (dest);
 }
