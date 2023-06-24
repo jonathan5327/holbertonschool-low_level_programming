f8fb9b65b97daf3001a0eba381ff0eb58bc2d085
root <6772@holbertonstudents.com>
2023-06-23 18:29:17 -0700

    task 0

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -3,22 +3,23 @@
  * _strcat - Write a function that concatenates two strings.
  * @i: character
  * @j: character
+ * @dest: pointer
+ * @src: pointer
  */
 char *_strcat(char *dest, char *src)
 
 {
-	int i=0, j=0;
+	int i = 0, j = 0;
 
 		while (dest[i++])
 		{
 			j++;
 		}
-		while (src[j++])
-		{
-			dest[i] = src[j];
-			i++;
-		}	
-		return dest;
+		for (i = 0; src[i]; i++)
+			dest[j++] = src[i];
+
+
+		return (dest);
 
 }
 	
